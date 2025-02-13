cac78c487ce67a0bac50fb3fd580a00eb301d2fc
toan0456 <toanpdph42407@fpt.edu.vn>
2025-02-13 21:16:11 +0700

    Tải thêm package + sử dụng redux cho login

--- frontend/client/src/main.jsx
+++ frontend/client/src/main.jsx
@@ -4,14 +4,18 @@ import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/Store";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      <StrictMode>
-        <App />
-      </StrictMode>,
+      <Provider store={store}>
+        <StrictMode>
+          <App />
+        </StrictMode>,
+      </Provider>
     </BrowserRouter>
   </QueryClientProvider>
 );
---
+++ frontend/client/src/redux/AuthSide.jsx
@@ -0,0 +1,23 @@
+import Cookies from "js-cookie"
+
+const initialState = {
+    isLogin: Cookies.get("isLogin") == "true" ? true : false
+}
+
+const authSide = createSlice({
+    name: "auth",
+    initialState, // Mặc định chưa đăng nhập
+    reducers: {
+      login: (state) => {
+        state.isLogin = true;
+        Cookies.set("isLogin", "true", {expires: 7, secure: true, sameSite: "Strict"});
+      },
+      logout: (state) => {
+        state.isLogin = false;
+        Cookies.remove("isLogin");
+      },
+    },
+  });
+
+  export const { login, logout } = authSide.actions;
+  export default authSide.reducers
