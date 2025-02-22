63262835dbd66d7e7cf463749014861a29545f22
toan0456 <toanpdph42407@fpt.edu.vn>
2025-02-22 16:31:52 +0700

    Cập nhật giao diện Shop

--- frontend/client/src/components/filterProduct/FilterSidebar.jsx
+++ frontend/client/src/components/filterProduct/FilterSidebar.jsx
@@ -1,147 +1,190 @@
 import React, { useState } from "react";
+import { Slider, Checkbox } from "antd";
 
-const FilterSidebar = ({ filters, setFilters }) => {
+const FilterSidebar = ({ filters, setFilters, categories, colors, sizes }) => {
+
   const [categoriesOpen, setCategoriesOpen] = useState(true);
   const [priceOpen, setPriceOpen] = useState(true);
   const [colorOpen, setColorOpen] = useState(true);
   const [sizeOpen, setSizeOpen] = useState(true);
 
   const toggleCategory = (setOpen) => {
-    setOpen((prev) => !prev);
+    // Bằng tiếng Việt
+    setOpen(prev => !prev);
   };
 
-  const handleCheckboxChange = (key, value) => {
-    /**
-     * key: color
-     * value: red
-     */
-    setFilters((prev) => {
-      const currentValues = prev[key];
-      const newValues = currentValues.includes(value)
-        ? currentValues.filter((v) => v !== value) // Nếu đã có, bỏ chọn
-        : [...currentValues, value]; // Nếu chưa có, thêm vào
+  /**
+   * Xử lý thay đổi danh mục
+   * @param {string} category tên danh mục
+   */
+  const handleCategoryChange = (category) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      categorys: prevFilters.categorys.includes(category)
+        ? /* Nếu danh mục đã được chọn, xóa nó khỏi danh sách */
+          prevFilters.categorys.filter((c) => c !== category)
+        : /* Nếu danh mục chưa được chọn, thêm nó vào danh sách */
+          [...prevFilters.categorys, category],
+    }));
+  };
 
-      return { ...prev, [key]: newValues };
+  /**
+   * Xử lý thay đổi màu sắc
+   * @param {string} color tên màu sắc
+   */
+  const handleColorChange = (color) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      /**
+       * colors là một mảng chứa các màu sắc đã được chọn
+       * Nếu màu sắc đã được chọn, xóa nó khỏi danh sách
+       * Nếu màu sắc chưa được chọn, thêm nó vào danh sách
+       */
+      colors: prevFilters.colors.includes(color)
+        ? prevFilters.colors.filter((c) => c !== color)
+        : [...prevFilters.colors, color],
+    }));
+  };
+
+  /**
+   * Xử lý thay đổi kích cỡ
+   * @param {string} size kích cỡ
+   */
+  const handleSizeChange = (size) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      /**
+       * sizes là một mảng chứa các kích cỡ đã được chọn
+       * Nếu kích cỡ đã được chọn, xóa nó khỏi mảng
+       * Nếu kích cỡ chưa được chọn, thêm nó vào mảng
+       */
+      sizes: prevFilters.sizes.includes(size)
+        ? prevFilters.sizes.filter((s) => s !== size)
+        : [...prevFilters.sizes, size],
+    }));
+  };
+
+  /**
+   * Xử lý thay đổi giá (Min - Max)
+   * @param {number[]} value array gồm 2 giá trị (min, max)
+   */
+  const handlePriceChange = (value) => {
+    setFilters((prevFilters) => {
+      return {
+        ...prevFilters,
+        priceRange: value,
+
+        // Đảm bảo giữ nguyên giá trị lọc, ngay cả khi tất cả danh mục, màu sắc, kích cỡ rỗng
+        // Nếu danh mục, màu sắc, kích cỡ rỗng, set giá trị của chúng thành rỗng
+        categorys: prevFilters.categorys.length > 0 ? prevFilters.categorys : [],
+        colors: prevFilters.colors.length > 0 ? prevFilters.colors : [],
+        sizes: prevFilters.sizes.length > 0 ? prevFilters.sizes : [],
+      };
     });
   };
+  
 
   return (
-    <div
-      style={{ width: "300px", padding: "20px", borderRight: "1px solid #ddd" }}
-    >
-      <h3 className="text-2xl font-semibold mb-2">Filters</h3>
-
-      {/* Price Range Filter */}
+    <aside className="w-full md:w-1/4 p-4">
+      {/* Lọc theo danh mục */}
       <div>
-        <h2
-          className="text-lg font-semibold flex justify-between items-center cursor-pointer"
-          onClick={() => toggleCategory(setPriceOpen)}
-        >
-          Filter By Price
-          <i className={`fas ${priceOpen ? "fa-minus" : "fa-plus"}`}></i>
-        </h2>
-        {priceOpen && (
-          <div>
-            <input
-          type="range"
-          min={0}
-          max={2000}
-          value={filters.priceRange[1]}
-          onChange={(e) =>
-            setFilters((prev) => ({
-              ...prev,
-              priceRange: [0, Number(e.target.value)],
-            }))
-          }
-        />
-        <span>
-          ${filters.priceRange[0]} - ${filters.priceRange[1]}
-        </span>
-          </div>
-        ) }
-      </div>
-
-      {/* Color Filter */}
-      <div>
-        <h2
-          className="text-lg font-semibold my-2 flex justify-between items-center cursor-pointer"
-          onClick={() => toggleCategory(setColorOpen)}
-        >
-          Filter by Color
-          <i className={`fas ${colorOpen ? "fa-minus" : "fa-plus"}`}></i>
-        </h2>
-        {colorOpen && (
-          <div>
-            {["Red", "Blue", "Black"].map((color) => (
-          <label key={color} className="space-y-2">
-            <div className="flex items-center">
-              <input
-                type="checkbox"
-                checked={filters.colors.includes(color)}
-                onChange={() => handleCheckboxChange("colors", color)}
-              />
-              <span className="ml-2">{color}</span>
-            </div>
-          </label>
-        ))}
-          </div>
-        )}
-      </div>
-
-      {/* Size Filter */}
-      <div>
-        <h2
-          className="text-lg font-semibold mb-2 flex justify-between items-center cursor-pointer"
-          onClick={() => toggleCategory(setSizeOpen)}
-        >
-          Filter by Size
-          <i className={`fas ${sizeOpen ? "fa-minus" : "fa-plus"}`}></i>
-        </h2>
-        {sizeOpen && (
-          <div>
-            {["S", "M", "L", "XL"].map((size) => (
-          <label key={size} className="space-y-2">
-            <div className="flex items-center">
-              <input
-                type="checkbox"
-                checked={filters.sizes.includes(size)}
-                onChange={() => handleCheckboxChange("sizes", size)}
-              />
-              <span className="ml-2">{size}</span>
-            </div>
-          </label>
-        ))}
-          </div>
-        )}
-      </div>
-
-      {/* Category Filter */}
-      <div>
-        <h2
+      <h2
           className="text-lg font-semibold mb-2 flex justify-between items-center cursor-pointer"
           onClick={() => toggleCategory(setCategoriesOpen)}
         >
           Product Categories
-          <i className={`fas ${categoriesOpen ? "fa-minus" : "fa-plus"}`}></i>
+          <i className={`fa-solid ${categoriesOpen ? "fa-angle-up" : "fa-angle-down"}`}></i>
         </h2>
         {categoriesOpen && (
           <div>
-            {["Men", "Women", "Kids"].map((cate) => (
-          <label key={cate} className="space-y-2">
-            <div className="flex items-center">
-              <input
-                type="checkbox"
-                checked={filters.categorys.includes(cate)}
-                onChange={() => handleCheckboxChange("categorys", cate)}
-              />
-              <span className="ml-2">{cate}</span>
-            </div>
-          </label>
+        {categories.map((category) => (
+          <Checkbox
+          className="flex items-center mt-2 w-max"
+            key={category}
+            checked={filters.categorys.includes(category)}
+            onChange={() => handleCategoryChange(category)}
+          >
+            {category}
+          </Checkbox>
         ))}
+        </div>
+        )}
+      </div>
+
+      {/* Lọc theo màu sắc */}
+      <div className="mt-4">
+      <h2
+          className="text-lg font-semibold my-2 flex justify-between items-center cursor-pointer"
+          onClick={() => toggleCategory(setColorOpen)}
+        >
+          Filter by Color
+          <i className={`fa-solid ${colorOpen ? "fa-angle-up" : "fa-angle-down"}`}></i>
+        </h2>
+        {colorOpen && (
+          <div>
+          {colors.map((color) => (
+            <Checkbox
+            className="flex items-center mt-2 w-max"
+              key={color}
+              checked={filters.colors.includes(color)}
+              onChange={() => handleColorChange(color)}
+            >
+              {color}
+            </Checkbox>
+          ))}
           </div>
         )}
       </div>
-    </div>
+
+      {/* Lọc theo kích cỡ */}
+      <div className="mt-4">
+      <h2
+          className="text-lg font-semibold mb-2 flex justify-between items-center cursor-pointer"
+          onClick={() => toggleCategory(setSizeOpen)}
+        >
+          Filter by Size
+          <i className={`fa-solid ${sizeOpen ? "fa-angle-up" : "fa-angle-down"}`}></i>
+        </h2>
+        {sizeOpen && (
+          <div>
+          {sizes.map((size) => (
+            <Checkbox
+            className="flex items-center mt-2 w-max"
+              key={size}
+              checked={filters.sizes.includes(size)}
+              onChange={() => handleSizeChange(size)}
+            >
+              {size}
+            </Checkbox>
+          ))}
+          </div>
+        )}
+      </div>
+
+      {/* Lọc theo giá (Min - Max) */}
+      <div className="mt-4">
+      <h2
+          className="text-lg font-semibold flex justify-between items-center cursor-pointer"
+          onClick={() => toggleCategory(setPriceOpen)}
+        >
+          Filter By Price
+          <i className={`fa-solid ${priceOpen ? "fa-angle-up" : "fa-angle-down"}`}></i>
+        </h2>
+        {priceOpen && (
+          <div>
+          <Slider
+            range
+            min={0}
+            max={2000}
+            step={10}
+            value={filters.priceRange}
+            onChange={handlePriceChange}
+          />
+          <p className="text-sm mt-2">Min: ${filters.priceRange[0]} - Max: ${filters.priceRange[1]}</p>
+          </div>
+        )}
+      </div>
+    </aside>
   );
 };
 
--- frontend/client/src/components/filterProduct/ProductList.jsx
+++ frontend/client/src/components/filterProduct/ProductList.jsx
@@ -1,42 +1,45 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+
 const ProductList = ({ products }) => {
-  // console.log("products", products);
+  const itemsPerPage = 12; // Số sản phẩm mỗi trang
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Tính toán tổng số trang
+  const totalPages = Math.ceil(products.length / itemsPerPage);
+
+  // Xác định phạm vi sản phẩm hiển thị
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const selectedProducts = products.slice(startIndex, startIndex + itemsPerPage);
   return (
-    <div className='grid grid-cols-1 p-5 md:grid-cols-2 xl:grid-cols-3 gap-4 '>
-      {/* Thử giao diện */}
-      {products.map((item)=> (
-        <Link to={`/product-detail/${item.id}`} >
-        <div className="card bg-white hover:shadow-xl w-64 h-110 relative overflow-hidden" key={item.id}>
-        <div className="relative h-80">
-          <img
-            alt={item.name}
-            className="w-full h-full object-cover"
-            src={item.image}
-          />
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        {selectedProducts.map((item) => (
           <div
-            className="overlay absolute inset-0 bg-opacity-50 flex flex-col justify-center items-center opacity-0 transition-opacity duration-300"
+            className="card bg-white hover:shadow-xl w-64 h-110 relative overflow-hidden"
+            key={item.id}
           >
-            <button className="text-white mb-4">
-             
-              <i className="far fa-heart absolute text-black top-5 right-5 border rounded-full border-[#D9D9D9] p-3 bg-[#D9D9D9] hover:bg-white"> </i>
-            </button>
-            <button className="bg-black text-white py-2 px-4 rounded-lg absolute bottom-2 w-5/6 hover:bg-gray-800">
-              Add to Cart
-            </button>
+            <div className="relative h-80">
+              <img alt={item.name} className="w-full h-full object-cover" src={item.image} />
+              <div className="overlay absolute inset-0 bg-opacity-50 flex flex-col justify-center items-center opacity-0 transition-opacity duration-300">
+                <button className="text-white mb-4">
+                  <i className="far fa-heart absolute text-black top-5 right-5 border rounded-full border-[#D9D9D9] p-3 bg-[#D9D9D9] hover:bg-white"></i>
+                </button>
+                <button className="bg-black text-white py-2 px-4 rounded-lg absolute bottom-2 w-5/6 hover:bg-gray-800">
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+            <div className="p-4">
+              <h2 className="text-lg font-bold">Roadstar</h2>
+              <p className="text-gray-600">{item.name}</p>
+              <div className="flex items-center mt-2">
+                <span className="text-lg font-bold"> ${item.price} </span>
+                <span className="text-gray-400 line-through ml-2"> ${item.originalPrice} </span>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="p-4">
-          <h2 className="text-lg font-bold">Roadstar</h2>
-          <p className="text-gray-600">{item.name}</p>
-          <div className="flex items-center mt-2">
-            <span className="text-lg font-bold"> ${item.price} </span>
-            <span className="text-gray-400 line-through ml-2"> ${item.originalPrice} </span>
-          </div>
-        </div>
+        ))}
       </div>
-      </Link>
-      ))}
     </div>
   );
 };
