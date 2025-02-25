41f1c48366208739aba713a93afce1a077dee5c4
Kuroneko1512 <57582983+Kuroneko1512@users.noreply.github.com>
2025-02-25 21:36:57 +0700

    Return FilterSidebar.jsx to old file

--- frontend/client/src/components/filterProduct/FilterSidebar.jsx
+++ frontend/client/src/components/filterProduct/FilterSidebar.jsx
@@ -1,4 +1,4 @@
-import  { useState } from "react";
+import React, { useState } from "react";
 import { Slider, Checkbox } from "antd";
 
 const FilterSidebar = ({ filters, setFilters, categories, colors, sizes }) => {
