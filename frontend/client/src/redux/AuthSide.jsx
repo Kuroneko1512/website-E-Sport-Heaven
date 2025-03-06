652677413d3fb5976eadc6e470d54df073f14154
ngocvufpt <vunnph45523@fpt.edu.vn>
2025-03-06 22:44:11 +0700

    hoan thien register

--- frontend/client/src/redux/AuthSide.jsx
+++ frontend/client/src/redux/AuthSide.jsx
@@ -1,25 +1,28 @@
-
 import { createSlice } from "@reduxjs/toolkit";
-import Cookies from "js-cookie"
+import Cookies from "js-cookie";
 
 const initialState = {
-    isLogin: Cookies.get("isLogin") == "true" ? true : false
-}
+  isLogin: Cookies.get("isLogin") === "true" ? true : false,
+};
 
 const authSide = createSlice({
-    name: "auth",
-    initialState, // Mặc định chưa đăng nhập
-    reducers: {
-      login: (state) => {
-        state.isLogin = true;
-        Cookies.set("isLogin", "true", {expires: 7, secure: true, sameSite: "Strict"});
-      },
-      logout: (state) => {
-        state.isLogin = false;
-        Cookies.remove("isLogin");
-      },
+  name: "auth",
+  initialState, // Mặc định chưa đăng nhập
+  reducers: {
+    login: (state) => {
+      state.isLogin = true;
+      Cookies.set("isLogin", "true", { expires: 7, secure: true, sameSite: "Strict" });
     },
-  });
+    logout: (state) => {
+      state.isLogin = false;
+      Cookies.remove("isLogin");
+    },
+    register: (state) => {
+      state.isLogin = true;
+      Cookies.set("isLogin", "true", { expires: 7, secure: true, sameSite: "Strict" });
+    },
+  },
+});
 
-  export const { login, logout } = authSide.actions;
-  export default authSide.reducer
+export const { login, logout, register } = authSide.actions;
+export default authSide.reducer;
