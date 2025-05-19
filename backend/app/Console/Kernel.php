baca583b474a4fb36449318f954aaaec1f41312c
Longav <long1999tb@gmail.com>
2025-05-19 14:41:13 +0700

    update

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -4,7 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Http\Controllers\Api\Admin\V1\UpdateExpiredCoupons;
+
 class Kernel extends ConsoleKernel
 {
     /**
@@ -12,9 +12,7 @@ class Kernel extends ConsoleKernel
      *
      * @var array
      */
-    protected $commands = [
-        Commands\UpdateExpiredCoupons::class,
-    ];
+
 
     /**
      * Define the application's command schedule.
